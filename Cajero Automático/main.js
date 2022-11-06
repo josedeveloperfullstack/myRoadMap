7914af5df819b4e20075ea94d804b7e1f0dbba1f
Jose Alfredo Aguado <josealfredoaguadomesa@gmail.com>
2022-11-05 23:59:00 -0500

    addingchangeses

--- Cajero Automático/main.js
+++ Cajero Automático/main.js
@@ -1,7 +1,12 @@
-const formularioUser = 'juan@email.com'
+let p = document.querySelector('#personas');
+console.log(p)
+
+console.log(document)
+
+
+/*const formularioUser = 'juan@email.com'
 const formularioPassword ='1234'
 
 //for of
 for (const usuario of usuarioBD){
-    console.log(usuario.email, 'email')
-}
+    console.log(usuario.email, 'email')*/
